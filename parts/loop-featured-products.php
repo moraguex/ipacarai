c8f05ac3d2a2b2d76adf4b93e17a18464b25f245
Andres Moraga <amoragap28@gamil.com>
2017-04-21 03:46:17 -0600

    fixes custom post type mismatch

--- parts/loop-featured-products.php
+++ parts/loop-featured-products.php
@@ -3,7 +3,7 @@
 
   	    // Define the query
   	    $args = array(
-  	        'post_type' => 'productos',
+  	        'post_type' => 'product',
   	      	'posts_per_page' => '6',
   	      	'meta_key'   => 'featured-home',
 			'meta_value' => 1,
