cd360b62a40a6a3af471660d8d2649e67a10348a
Andres Moraga <amoragap28@gamil.com>
2017-04-21 03:19:42 -0600

    removes container from contact from part

--- parts/content-contact-form.php
+++ parts/content-contact-form.php
@@ -1,5 +1,3 @@
-<section class="contact-form-container">
-	<h2><?php the_field('contact-section-title'); ?></h2>
+<h2><?php the_field('contact-section-title'); ?></h2>
 
-	<?php echo do_shortcode('[contact-form-7 id="68" title="Formulario de contacto" html_class="row"]'); ?>
-</section>
+<?php echo do_shortcode('[contact-form-7 id="68" title="Formulario de contacto" html_class="row"]'); ?>
