369ca83034def7d7461790a86471b285806f7d2b
Andres Moraga <amoragap28@gamil.com>
2017-04-21 03:06:38 -0600

    adds contact info part

---
+++ parts/content-contact-info.php
@@ -0,0 +1,7 @@
+<div class="contact-info">
+	<h3><?php the_field('contact-information-title'); ?></h3>
+	<address><span class="fa fa-map-marker" aria-hidden="true"></span> <span><?php the_field('contact-address'); ?></span></address>
+	<address><span class="fa fa-phone" aria-hidden="true"></span> <span><?php echo __('Phone:', 'ipacaraiwp'); the_field('phone-number'); ?>. <?php echo __('Fax:', 'ipacarai'); the_field('fax'); ?>.</span></address>
+	<address><span class="fa fa-envelope email" aria-hidden="true"></span> <a href="mailto:ipacarai@racsa.co.cr"><?php the_field('email'); ?></a></address>
+	<address><span class="fa fa-user" aria-hidden="true"></span> <span><?php echo __('General Manager:', 'ipacaraiwp'); ?> <br><?php the_field('contact-person'); ?></span></address>
+</div>
