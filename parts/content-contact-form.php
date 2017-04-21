39f60c24140f043111171b080f62ded3f30e22b8
Andres Moraga <amoragap28@gamil.com>
2017-04-21 05:02:21 -0600

    updates contact form shortcode id

--- parts/content-contact-form.php
+++ parts/content-contact-form.php
@@ -1,3 +1,3 @@
 <h2><?php the_field('contact-section-title'); ?></h2>
 
-<?php echo do_shortcode('[contact-form-7 id="68" title="Formulario de contacto" html_class="row"]'); ?>
+<?php echo do_shortcode('[contact-form-7 id="9" title="Formulario de contacto" html_class="row"]'); ?>
