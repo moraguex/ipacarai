5ba3d7e3bbcfe353306292dcc2b23992e7eff248
Andres Moraga <amoragap28@gamil.com>
2017-04-21 00:07:06 -0600

    adds translation support

---
+++ assets/translation/translation.php
@@ -0,0 +1,8 @@
+<?php
+
+// Adding Translation Option
+add_action('after_setup_theme', 'load_translations');
+function load_translations(){
+	load_theme_textdomain( 'ipacarai', get_template_directory() .'/assets/translation' );
+}
+?>
