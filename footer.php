1112776f4a26c4b4462b312fe46d0c9975f286c0
Andres Moraga <amoragap28@gamil.com>
2017-04-21 17:29:36 -0600

    fixes footer logos src

--- footer.php
+++ footer.php
@@ -3,8 +3,8 @@
 	    <div class="container">
 				<div class="row">
 					<div class="col-xs-6 col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ipacarai-logo.png" class="img-responsive ipacarai" alt="Ipacarai Logo"></div>
-          <div class="col-xs-6 col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>assets/images/escud-logo.png" class="img-responsive escud" alt="Escud Logo"></div>
-					<div class="col-xs-12 col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>assets/images/pyme-logo.png" class="img-responsive pyme" alt="Pyme Logo"></div>
+          <div class="col-xs-6 col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/escud-logo.png" class="img-responsive escud" alt="Escud Logo"></div>
+					<div class="col-xs-12 col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/pyme-logo.png" class="img-responsive pyme" alt="Pyme Logo"></div>
 					<div class="col-xs-12">
 						<?php ipacarai_footer_links(); ?>
 					</div>
