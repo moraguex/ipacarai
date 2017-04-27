6b97e487a5fcc49808384805f30f26e3346db2a7
Andres Moraga <amoragap28@gamil.com>
2017-04-27 03:56:21 -0600

    fixes footer logos

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
   <footer class="main-footer" role="contentinfo">
 		<div class="main-footer-top">
 	    <div class="container">
-				<div class="row">
-					<div class="col-xs-6 col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ipacarai-logo.png" class="img-responsive ipacarai" alt="Ipacarai Logo"></div>
-          <div class="col-xs-6 col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/escud-logo.png" class="img-responsive escud" alt="Escud Logo"></div>
-					<div class="col-xs-12 col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/pyme-logo.png" class="img-responsive pyme" alt="Pyme Logo"></div>
+				<div class="row footer-logos">
+					<div class="col-xs-6 col-sm-4"><span class="ipacarai-logo">Distribuidora Ipacara&iacute;</span></div>
+          <div class="col-xs-6 col-sm-4 col-sm-push-4"><span class="escud-logo">Escud</span></div>
+					<div class="col-xs-12 col-sm-4 col-sm-pull-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/pyme-logo.png" class="img-responsive pyme-logo" alt="Pyme Logo"></div>
 				</div>
         <div class="row">
           <div class="col-xs-12">
