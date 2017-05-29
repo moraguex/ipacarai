b6e49c96bd0dff7cece08e5b377c48f44dd68bad
Andres Moraga <moraguex@Andress-MacBook-Pro.local>
2017-05-29 13:54:50 -0600

    sets product presentations to be loaded from a custom field instead of being added to post content

--- single-producto.php
+++ single-producto.php
@@ -13,22 +13,39 @@
 		              <div class="col-xs-12 col-md-7">
 
 		              	<?php get_template_part( 'parts/content', 'product-banner' ); ?>
-		                
+
 		              </div>
 
 		              <div class="col-xs-12 col-md-5">
-		                  <div class="product-description">
-		                    <h2><?php the_title(); ?></h2>
-		                    <?php the_content(); ?>
-		                  </div>
+										<div class="product-description">
+											<h2><?php the_title(); ?></h2>
+
+											<h3>Presentaciones del producto:</h3>
+											<ul>
+												<?php
+													$variations = get_field('product-presentations');
+
+													$variations_array = explode(',', $variations);
+
+													$product_name = get_field('brand') .' '. get_the_title();
+
+													foreach ( $variations_array as $i ) {
+														echo '<li>'. $product_name .' de <strong>'. $i .'</strong></li>';
+													}
+												?>
+											</ul>
+
+											<h3>Descripci&oacute;n:</h3>
+											<?php the_content(); ?>
+										</div>
 		              </div>
 		            </div>
 		          </div>
 		        </section>
 
-		        
+
 		       	<?php get_template_part( 'parts/content', 'related-products' ); ?>
-					
+
 		    </article>
 
 		    <?php endwhile; // End of the loop.
