9c874ecdbfd00b86751dca38b8f5787976d294af
Andres Moraga <moraguex@Andress-MacBook-Pro.local>
2017-06-04 18:52:27 -0600

    separates fax in a new line from the contact info box

--- parts/content-contact-info.php
+++ parts/content-contact-info.php
@@ -1,6 +1,6 @@
 <div class="contact-info">
 	<h3><?php the_field('contact-information-title'); ?></h3>
 	<address><span class="fa fa-map-marker" aria-hidden="true"></span> <span><?php the_field('contact-address'); ?></span></address>
-	<address><span class="fa fa-phone" aria-hidden="true"></span> <span><?php echo __('Tel&eacute;fono: ', 'ipacaraiwp'); the_field('phone-number'); ?>. <?php echo __('Fax: ', 'ipacaraiwp'); the_field('fax'); ?>.</span></address>
+	<address><span class="fa fa-phone" aria-hidden="true"></span> <span><?php echo __('Tel&eacute;fonos: ', 'ipacaraiwp'); the_field('phone-number'); ?></span><br><span><?php echo __('Fax: ', 'ipacaraiwp'); the_field('fax'); ?></span></address>
 	<address><span class="fa fa-envelope email" aria-hidden="true"></span> <a href="mailto:ipacarai@racsa.co.cr"><?php the_field('email'); ?></a></address>
 </div>
