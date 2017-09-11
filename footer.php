3131c18ea0f4524eb7d1fbe4faa841e32c0d572d
Andrés Moraga <amoragap28@gmail.com>
2017-09-10 19:01:19 -0600

    sets new ipacarai logo and changes footer color

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 		<div class="main-footer-top">
 	    <div class="container">
 				<div class="row footer-logos">
-					<div class="col-xs-6 col-sm-4"><span class="ipacarai-logo">Distribuidora Ipacara&iacute;</span></div>
+					<div class="col-xs-6 col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ipacarai-logo.jpg" class="img-responsive ipacarai-logo" alt="Ipacarai Logo"></div>
           <div class="col-xs-6 col-sm-4 col-sm-push-4"><span class="escud-logo">Escud</span></div>
 					<div class="col-xs-12 col-sm-4 col-sm-pull-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/pyme-logo.png" class="img-responsive pyme-logo" alt="Pyme Logo"></div>
 				</div>
